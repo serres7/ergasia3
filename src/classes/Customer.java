04e982bc74ede78e2d54167737667adbe126661b
serres7 <ioannis.karnezis@yahoo.com>
2015-12-23 22:16:03 +0200

    Add Cunstractor

--- src/classes/Customer.java
+++ src/classes/Customer.java
@@ -1,8 +1,21 @@
 package classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer 
 {
-	String id;
-	String name;
+	//Attributes
+	private String id;
+	private String name;
+	private List invoiceList;
+	
+	public Customer( String id, String name )
+	{
+		this.id = id;
+		this.name = name;
+		this.invoiceList = new ArrayList();
+		
+	}//Contructor
 
 }
